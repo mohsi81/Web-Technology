b93973b263ac5fc9177623a9a318444a9f115966
mohsi81 <mohsinmughal28114@gmail.com>
2025-12-13 19:45:37 +0500

    Add files via upload

---
+++ lab8.js
@@ -0,0 +1,30 @@
+let color=["red","blue","yellow","orange"];
+let count=0;
+document.getElementsByClassName("change_color")[0].addEventListener("click",function(){
+    document.querySelector("body").style.background=color[count];
+    count=(count+1)%color.length;
+});
+
+document.getElementsByClassName("change_shape")[0].addEventListener("mouseover",function(){
+    this.style.height="200px";
+    this.style.background="orange";
+});
+
+document.getElementsByClassName("change_shape")[0].addEventListener("mouseout",function(){
+    this.style.height="50px";
+    this.style.background="rgb(155, 130, 247)"
+});
+
+
+
+var clicked=true;
+document.getElementsByClassName("change_Text")[0].addEventListener("click",function(){
+    if(clicked){
+        this.textContent= "Text Changed";
+        
+    }
+    else{
+        this.textContent = "Click To Change Text";
+    }
+        clicked= !clicked;
+});
